77ecb5666166f8de457f5749cecc9a842968fb69
Clemens Christierson <clemensc@axis.com>
2025-12-03 11:04:27 +0100

    day 3 done

--- 3/main.py
+++ 3/main.py
@@ -10,7 +10,7 @@ def parse_input(i):
     return [[int(a) for a in n] for n in i.split()]
 
 
-def find_max(n):
+def find_max1(n):
     max1 = max(n[:-1])
     i = n.index(max1)
     max2 = max(n[i + 1 :])
@@ -21,7 +21,7 @@ def find_max2(n):
     res = ""
     searchspace = n
     i_min = -1
-    for i_max in range(-11, 1, 1):
+    for i_max in range(-11, 1):
         high = max(searchspace[: i_max or None])
         i_min = searchspace[: i_max or None].index(high)
         searchspace = searchspace[i_min + 1 :]
@@ -29,16 +29,16 @@ def find_max2(n):
     return int(res)
 
 
-sum = 0
-for n in parse_input(input):
-    sum += find_max2(n)
-print(sum)
+def find_max(n, l):
+    res = ""
+    searchspace = n
+    for i_max in range(-(l - 1), 1):
+        high = max(searchspace[: i_max or None])
+        i_min = searchspace[: i_max or None].index(high)
+        searchspace = searchspace[i_min + 1 :]
+        res += str(high)
+    return int(res)
 
-# find_max2(parse_input(input)[2])
 
-
-# 987654321111 + 811111111119 + 434234234278 + 888911112111 = 3121910778619
-# 987654321111
-# 811111111119
-# 434444444478
-# 888919192192
+print(sum([find_max(n, 2) for n in parse_input(input)]))
+print(sum([find_max(n, 12) for n in parse_input(input)]))
